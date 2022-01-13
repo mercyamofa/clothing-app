e8ef789d51dfe3b36cc6e776470d336f82590874
unknown <mercyamofa3@gmail.com>
2022-01-12 19:15:10 -0800

    broken our homepage into smaller component, finishd styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory.component";
+
+import "./homepage.Styles.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
